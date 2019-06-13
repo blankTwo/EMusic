da6371a62f82577add93be0aaa5e012858486e6d
blankgrid <blankgrid.lsy@gmail.com>
2019-06-14 01:01:59 +0800

    初始化界面

--- main.ts
+++ main.ts
@@ -7,32 +7,34 @@ const args = process.argv.slice(1);
 serve = args.some(val => val === '--serve');
 
 function createWindow() {
-
   const electronScreen = screen;
   const size = electronScreen.getPrimaryDisplay().workAreaSize;
 
   // Create the browser window.
   win = new BrowserWindow({
-    x: 0,
-    y: 0,
-    width: size.width,
-    height: size.height,
+    width: size.width / 2,
+    height: size.height / 2,
     webPreferences: {
       nodeIntegration: true,
     },
+    frame: false,
+    minHeight: 650,
+    minWidth: 1024,
   });
 
   if (serve) {
     require('electron-reload')(__dirname, {
-      electron: require(`${__dirname}/node_modules/electron`)
+      electron: require(`${__dirname}/node_modules/electron`),
     });
     win.loadURL('http://localhost:4200');
   } else {
-    win.loadURL(url.format({
-      pathname: path.join(__dirname, 'dist/index.html'),
-      protocol: 'file:',
-      slashes: true
-    }));
+    win.loadURL(
+      url.format({
+        pathname: path.join(__dirname, 'dist/index.html'),
+        protocol: 'file:',
+        slashes: true,
+      }),
+    );
   }
 
   if (serve) {
@@ -46,11 +48,9 @@ function createWindow() {
     // when you should delete the corresponding element.
     win = null;
   });
-
 }
 
 try {
-
   // This method will be called when Electron has finished
   // initialization and is ready to create browser windows.
   // Some APIs can only be used after this event occurs.
@@ -72,7 +72,6 @@ try {
       createWindow();
     }
   });
-
 } catch (e) {
   // Catch Error
   // throw e;
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { ipcRenderer, remote, BrowserWindow, app } from 'electron';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onMinEvent() {
+    remote.getCurrentWindow().minimize();
   }
 
+  onMaxEvent() {
+    const _max = remote.getCurrentWindow().isMaximized();
+    if (_max) {
+      remote.getCurrentWindow().unmaximize();
+    } else {
+      remote.getCurrentWindow().maximize();
+    }
+  }
+  onCloseEvent() {
+    remote.getCurrentWindow().close();
+  }
 }
